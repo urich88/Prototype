dc67e36d4cbf45220230adeaf61f57c206f5372f
Urich <ditcherwaker@gmail.com>
2015-07-04 20:10:55 -0500

    AI
    AI created

--- AI.cs
+++ AI.cs
@@ -3,13 +3,32 @@ using System.Collections;
 
 public class AI : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+  /*try of very simple AI, it will choose a rand number,
+   * depending of the number it gets, it will get a
+   * random attack and at a random robot*/
+
+  public int enemyAttack;
+
+  void GetAttack ()
+  {
+    enemyAttack = Random.Range(0,5);
+
+    if (enemyAttack == 1)
+    {
+      //base attack
+    }
+    if (enemyAttack == 2)
+    {
+      //medium attack
+    }
+    if (enemyAttack == 3)
+    {
+      //super attack
+    }
+    else
+    {
+      GetAttack();
+    }
+
+  }
 }
